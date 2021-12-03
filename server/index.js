f5cb8f700715f2fe40d6b5737a089b6446b413c6
Dallin Harkness <dalhark99@gmail.com>
2021-12-03 12:11:49 -0700

    finished

--- server/index.js
+++ server/index.js
@@ -22,4 +22,62 @@ app.get("/api/compliment", (req, res) => {
   
 });
 
+
+app.get("/api/fortune", (req, res) => {
+  const fortunes = ["A fresh start will put you on your way.", "A lifetime friend will be made soon", "A pleasant surprise is waiting for you!", "A smile is your personal welcome mat.", "You don't need luck, you are already a lucky person!"
+]
+
+  let randomFortune = Math.floor(Math.random() * fortunes.length)
+
+  let newFortunesOnClick = fortunes[randomFortune]
+
+  res.status(200).send(newFortunesOnClick)
+})
+
+
+app.get("/api/powers", (req, res) => {
+  const shootFire = ["FLAME ON!"]
+  res.status(200).send(shootFire)
+  const flameOn = alert(shootFire)
+
+ 
+
+})
+
+
+
+app.get("/api/passwords", (req, res) => {
+  const passwordsRandom = ['password','redbrickwall','1234icancount', 'youwillsmithneverguess0101', 'buildabearworkshopyeet33445', '0101110010101binarysucks', 'ifyouguessthisyourock99@#$','hitherenoob','yeetskeetskeetandyeet', 'mrpotatoheadisaG', 'myairjordansarefake$$$money'
+]
+
+  let funnyPasswords = Math.floor(Math.random() * passwordsRandom.length)
+
+  let clickAPassword = passwordsRandom[funnyPasswords]
+
+  res.status(200).send(clickAPassword)
+})
+
+app.get('/api/lottery', (req,res) => {
+  const sike = ['SIKE YOU GOT PUNKD!']
+  res.status(200).send(sike)
+})
+
+
+app.get("/api/luckyNumber", (req, res) => {
+  const luckyNum = (min,max) =>{
+    return Math.random() * (min - max) + min
+  }
+  res.status(200).send(luckyNum(0,100000))
+})
+
+app.get('/api/cowMoo', (req, res) =>{
+  if(addEventListener('mouseover',moo)){
+    res.status(200).send(moo)
+  }
+})
+
+
+
+
+
 app.listen(4000, () => console.log("Server running on 4000"));
